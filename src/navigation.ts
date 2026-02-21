f881fe0bfc983c71c51f58e6b751d90ad843e3c2
rparaschak <roman.paraschak@gmail.com>
2026-02-21 18:11:01 +0100

    blob navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,28 +2,10 @@ import { getBlogPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    // {
-    //   text: 'Services',
-    //   links: [
-    //     {
-    //       text: 'Supercharge Engineering Team with AI',
-    //       href: getPermalink('/services/supercharge-engineering-team-with-ai'),
-    //     },
-    //     {
-    //       text: 'Technical Due Diligence',
-    //       href: getPermalink('/services/technical-due-diligence'),
-    //     },
-    //   ],
-    // },
-    // {
-    //   text: 'Presentations',
-    //   links: [
-    //     {
-    //       text: 'Coding Agents 101: Foundation and Mindset',
-    //       href: getPermalink('/presentations/coding-agents-101'),
-    //     },
-    //   ],
-    // },
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
+    },
   ],
   actions: [],
 };
